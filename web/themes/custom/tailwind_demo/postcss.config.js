de77b32cc4802f9bc3a9b673e9e31faf7c978c2d
deepanshu.varshney04 <deepanshu.varshney04@gmail.com>
2022-11-25 19:36:58 +0530

    Doing Design of Home Page Other Section

--- web/themes/custom/tailwind_demo/postcss.config.js
+++ web/themes/custom/tailwind_demo/postcss.config.js
@@ -1,7 +1,39 @@
-module.exports = {
+/* module.exports = {
   plugins: {
     'postcss-import': {},
+    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
+} */
+
+
+// postcss.config.js
+/* module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
+} */
+
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    'postcss-nesting': {},
+    tailwindcss: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+        'nesting-rules': true
+      }
+    }
+  }
 }
